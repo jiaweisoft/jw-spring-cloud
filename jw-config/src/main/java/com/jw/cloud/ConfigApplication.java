8cceaabaf00b377b1ac5d4d40aafb30f62005572
jiawei <jiawei@youxin.com>
2020-06-11 11:07:37 +0800

    config-1

--- jw-config/src/main/java/com/jw/cloud/ConfigApplication.java
+++ jw-config/src/main/java/com/jw/cloud/ConfigApplication.java
@@ -3,18 +3,19 @@ package com.jw.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @Auther: jiawei
  * @Date: 2020/5/28
  * @Description:
  */
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
 }
